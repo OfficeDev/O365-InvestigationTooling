877d0b985c3760c139b291e1a6cb0ec35c86a751
Brandon Koeller <brkoelle@microsoft.com>
2016-02-25 11:10:40 -0800

    Enable Mailbox Auditing Powershell Script

---
+++ EnableMailboxAuditing.ps1
@@ -0,0 +1,14 @@
+﻿#This script will enable non-owner mailbox access auditing on every mailbox in your tenancy
+#First, let's get us a cred!
+$userCredential = Get-Credential
+
+#This gets us connected to an Exchange remote powershell service
+$ExoSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $userCredential -Authentication Basic -AllowRedirection
+Import-PSSession $ExoSession
+
+#Enable global audit logging
+Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox"} | Set-Mailbox -AuditEnabled $true -AuditLogAgeLimit 365
+
+#Double-Check It!
+Get-Mailbox | Select Name, AuditEnabled, AuditLogAgeLimit
+
