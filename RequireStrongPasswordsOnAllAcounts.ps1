5a7442b321aa2eaaa735de7810f6fc8b94962084
Brandon Koeller <brkoelle@microsoft.com>
2016-07-25 15:08:02 -0700

    Adding remediations for FileSync blacklist strong passwords

---
+++ RequireStrongPasswordsOnAllAcounts.ps1
@@ -0,0 +1,41 @@
+﻿#This script will require all of your users to use a strong password, including the below criteria:
+#The password must contain at least one lowercase letter
+#The password must contain at least one uppercase letter
+#The password must contain at least one non-alphanumeric character
+#The password cannot contain any spaces, tabs, or line breaks
+#The length of the password must be 8-16 characters
+#The user name cannot be contained in the password
+
+$transcriptpath = ".\" + "RequireStrongPasswordsTranscript" + (Get-Date).ToString('yyyy-MM-dd') + ".txt"
+Start-Transcript -Path $transcriptpath
+
+#Import the right module to talk with AAD
+import-module MSOnline
+
+#First, let's get us a cred!
+$adminCredential = Get-Credential
+
+#This connects to Azure Active Directory
+Connect-MsolService -Credential $adminCredential
+
+#Go get all them users
+$allUsers = @()
+$allUsers = Get-MsolUser -All -EnabledFilter EnabledOnly | select ObjectID, UserPrincipalName, FirstName, LastName, StrongAuthenticationRequirements, StsRefreshTokensValidFrom, StrongPasswordRequired, LastPasswordChangeTimestamp | Where-Object {($_.UserPrincipalName -notlike "*#EXT#*")}
+
+#Iterate through the list and require a strong password
+foreach ($User in $allUsers)
+{
+    $thisUser = $User.UserPrincipalName
+    Write-Host "Configuring user to use a strong password: " $thisUser;
+    Set-MsolUser -UserPrincipalName $thisUser -StrongPasswordRequired $true
+}
+
+Write-Output "And just to double check, here is the current state of strong password enablement in your tenancy: "
+
+#Double checkin'!
+foreach ($User in $allUsers)
+{
+    Get-MsolUser -UserPrincipalName $User.UserPrincipalName | select UserPrincipalName, StrongPasswordRequired
+}
+
+Stop-Transcript
---
+++ SetODBSyncFileExtensionBlacklist.ps1
@@ -0,0 +1,34 @@
+﻿#This script will configure your SharePoint Online tenancy to block the syncing of files that are known ransomware.
+
+$transcriptpath = ".\" + "SetODBFileSyncBlacklistTranscript" + (Get-Date).ToString('yyyy-MM-dd') + ".txt"
+Start-Transcript -Path $transcriptpath
+
+$AdminCredential = Get-Credential
+
+#This gets us connected to a SPOnline remote powershell service
+Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking
+
+    Try
+    {
+        #First, let's construct the SPOAdmin URL from the user's admin email address
+        $SPOAdminURLBase = $AdminCredential.UserName.Split("@")[1] | Select-String -Pattern "([^.]+)" | Select-Object -Expand matches | Select-Object -Expand Value
+        $SPOAdminURL = "https://" + $SPOAdminURLBase + "-admin.sharepoint.com"
+
+        #Then, let's connect to the service!
+        Connect-SPOService -Url $SPOAdminURL -Credential $AdminCredential
+    }
+    Catch
+    {
+        Write-Log "  No SharePoint Online service set up for this account"
+        $ServiceInfo.Set_Item("SPO", "False")        
+
+    }
+
+Write-Output "You current Sync Client configuration is set to: "
+Get-SPOTenantSyncClientRestriction
+
+Set-SPOTenantSyncClientRestriction  -ExcludedFileExtensions "ecc;ezz;exx;zzz;xyz;aaa;abc;ccc;vvv;xxx;ttt;micro;encrypted;locked;crypto;crinf;r5a;XRNT;XTBL;crypt;R16M01D05;pzdc;good;RDM;RRK;encryptedRSA;crjoker;EnCiPhErEd;LeChiffre;0x0;bleep;1999;vault;HA3;toxcrypt;magic;SUPERCRYPT;CTBL;CTB2;locky;cryp1;zepto"
+
+Write-Output "Excellent! You have configured your tenancy to not sync files with at least some known ransomware file extensions. Periodically update the list and your blacklist."
+
+Stop-Transcript
