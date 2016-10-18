2b3fba9e270fe8fef9f3623e5e450fdb8c61b3b8
Brandon Koeller <brkoelle@microsoft.com>
2016-10-18 13:07:57 -0700

    Add files via upload

---
+++ InactiveUsersLast90Days.ps1
@@ -0,0 +1,25 @@
+﻿import-module MSOnline
+
+#Let's get us an admin cred!
+$userCredential = Get-Credential
+
+#This connects to Azure Active Directory
+Connect-MsolService -Credential $userCredential
+$ExoSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $userCredential -Authentication Basic -AllowRedirection
+Import-PSSession $ExoSession
+
+$startDate = (Get-Date).AddDays(-90).ToString('MM/dd/yyyy')
+$endDate = (Get-Date).ToString('MM/dd/yyyy')
+
+$allUsers = @()
+$allUsers = Get-MsolUser -All -EnabledFilter EnabledOnly | Select UserPrincipalName
+
+$loggedOnUsers = @()
+$loggedOnUsers = Search-UnifiedAuditLog -StartDate $startDate -EndDate $endDate -Operations UserLoggedIn, PasswordLogonInitialAuthUsingPassword, UserLoginFailed -ResultSize 5000
+
+$inactiveInLastThreeMonthsUsers = @()
+$inactiveInLastThreeMonthsUsers = $allUsers.UserPrincipalName | where {$loggedOnUsers.UserIds -NotContains $_}
+
+Write-Output "The following users have no logged in for the last 90 days:"
+Write-Output $inactiveInLastThreeMonthsUsers
+
