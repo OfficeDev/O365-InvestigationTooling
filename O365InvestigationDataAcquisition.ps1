679ae2665559f0afc98eb04d5a7326a557437e7d
Brandon Koeller <brkoelle@microsoft.com>
2017-01-18 13:48:15 -0800

    Update O365InvestigationDataAcquisition.ps1
    Adding two new claims!

--- O365InvestigationDataAcquisition.ps1
+++ O365InvestigationDataAcquisition.ps1
@@ -108,7 +108,7 @@ else
 $hours = @("00:00Z", "01:00Z", "02:00Z", "03:00Z", "04:00Z", "05:00Z", "06:00Z", "07:00Z", "08:00Z", "09:00Z", "10:00Z", "11:00Z", "12:00Z", "13:00Z", "14:00Z", "15:00Z", "16:00Z", "17:00Z", "18:00Z", "19:00Z", "20:00Z", "21:00Z", "22:00Z", "23:00Z", "23:59:59Z")
 foreach ($day in $days) { foreach ($hour in $hours) { $dateRange += $day + "T" + $hour; }}
 
-$workLoads = @("Audit.AzureActiveDirectory", "Audit.Exchange", "Audit.SharePoint")
+$workLoads = @("Audit.AzureActiveDirectory", "Audit.Exchange", "Audit.SharePoint", "Audit.General", "DLP.All")
 $subs = @()
 $global:blobs = @()
 $wlCount = @()
