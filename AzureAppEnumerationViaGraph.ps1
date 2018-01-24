5a8cb3c6785f57f6465d20fb519805e4dd05630c
Brandon Koeller <brkoelle@microsoft.com>
2018-01-24 15:14:21 -0800

    muncha buncha fixes

--- AzureAppEnumerationViaGraph.ps1
+++ AzureAppEnumerationViaGraph.ps1
@@ -1,15 +1,31 @@
-#Only need to run these if you don't have ADAL installed
-Install-PackageProvider -Name "NuGet"
-Register-PackageSource -Name NuGet -ProviderName NuGet -location https://www.nuget.org/api/v2/
-Get-PackageProvider -Name "NuGet" | Get-PackageSource
-Install-Package -Name "Microsoft.IdentityModel.Clients.ActiveDirectory"
-
+#########################################
+# AzureAppEnumerationViaGraph.ps1
+# 
+# Dumps all users, consent grants, assign apps, and app details in an Office 365 tenancy
+# Script requires user with Global Admin role assignment
+#
+#########################################
 param
 (
   [Parameter(Mandatory=$true)]
   $TenantName
 )
 
+#Let's see if you have ADAL installed and if not, give you some commands to run to get it installed
+$p = Get-Package -Name "Microsoft.IdentityModel.Clients.ActiveDirectory"
+if ($p -ne $null) { Write-Output "Great, looks like you have the correct package dependencies installed."}
+else 
+{
+    Write-Output "Looks like you need to install the Active Directory Authentication Library. Please run the following commands in an admininstrator elevated powershell window: "
+    Write-Output 'Install-PackageProvider -Name NuGet"'
+    Write-Output 'Register-PackageSource -Name NuGet -ProviderName NuGet -location https://www.nuget.org/api/v2/'
+    Write-Output 'Get-PackageProvider -Name "NuGet" | Get-PackageSource'
+    Write-Output 'Install-Package -Name "Microsoft.IdentityModel.Clients.ActiveDirectory"'
+}
+
+
+       
+#This function will acquire an OAuth token to authorize the graph api calls
 function GetAuthToken
 {
        param
@@ -39,23 +55,30 @@ $authHeader = @{
    "Content-Type" = "application\json"
 }
 
-#Get all dem users
+#Get all users in order to figure out which users have which app permissions assigned
 $userresource = "users"
 $uri = "https://graph.windows.net/myorganization/$($userresource)?api-version=1.6"
 $users = (Invoke-RestMethod -Uri $uri –Headers $authHeader –Method Get).value
 
 
-#For each user, retrieve a list of all the oauth consent grants they have made
-$consentgrants = @()
-foreach ($user in $users)
-{
-    Write-Output "Getting consent grants for user: " $user.userPrincipalName
-    $oauthconsentgrantresource = 'users/' + $user.userPrincipalName + '/oauth2PermissionGrants'
-    $uri = "https://graph.windows.net/myorganization/$($oauthconsentgrantresource)?api-version=1.6"
-    $consentgrants += (Invoke-RestMethod -Uri $uri –Headers $authHeader –Method Get).value
-}
+#Retrieve a list of all the oauth consent grants that have been made in the tenancy
+$allconsentgrants = @()
+$oauthconsentgrantresource = 'oauth2PermissionGrants'
+$uri = "https://graph.windows.net/myorganization/$($oauthconsentgrantresource)?api-version=1.6"
+$allconsentgrants = (Invoke-RestMethod -Uri $uri –Headers $authHeader –Method Get).value
 
-$appassingments = @()
+# This will retrieve consent grants for each individual, but will end up missing the admin consent grants.
+#$consentgrants = @()
+#foreach ($user in $users)
+#{
+#    Write-Output "Getting consent grants for user: " $user.userPrincipalName
+#    $oauthconsentgrantresource = 'users/' + $user.userPrincipalName + '/oauth2PermissionGrants'
+#    $uri = "https://graph.windows.net/myorganization/$($oauthconsentgrantresource)?api-version=1.6"
+#    $consentgrants += (Invoke-RestMethod -Uri $uri –Headers $authHeader –Method Get).value
+#}
+
+#For each user, go get a list of all the app permission assignments for that user
+$appassignments = @()
 foreach ($user in $users)
 {
     Write-Output "Getting app assignments for user: " $user.userPrincipalName
@@ -64,10 +87,10 @@ foreach ($user in $users)
     $appassignments += (Invoke-RestMethod -Uri $uri –Headers $authHeader –Method Get).value
 }
 
-
 #For each of the Assigned Applications, reduce the list to just the unique ones, then go get details about each of those applications
 $assignedAppGUIDS = $appassignments.resourceId | sort -Unique
 
+#For each application with permissions, go get details about that application
 $appDetails = @();
 foreach ($app in $assignedAppGUIDS)
 {
@@ -81,10 +104,11 @@ foreach ($app in $assignedAppGUIDS)
 $users | Export-Csv .\allusers_$((Get-Date).ToString('MM-dd-yyyy')).csv -NoTypeInformation
 
 #These are all the consent grant service principals in the directory for your users. Evaluate this to understand excessive scoped applications
-$consentgrants | Export-Csv .\allconsentgrants_$((Get-Date).ToString('MM-dd-yyyy')).csv -NoTypeInformation
+$allconsentgrants | Export-Csv .\allconsentgrants_$((Get-Date).ToString('MM-dd-yyyy')).csv -NoTypeInformation
 
 #these are all the assigned applications privileges in the directory for your users. Evaluate this to understand when an app got access to your user's data
 $appassignments | Export-Csv .\appassignments_$((Get-Date).ToString('MM-dd-yyyy')).csv -NoTypeInformation
 
 #These are details about all the apps that have assigned privileges. Evaluate this for suspicious URLs and names
 $appDetails | Export-Csv .\allappdetails_$((Get-Date).ToString('MM-dd-yyyy')).csv -NoTypeInformation
+
