bb91a8bc46e7a48e276f7d16ec5d016cd0c24258
Kevin C <PsychoData91@gmail.com>
2018-12-16 18:14:23 -0500

    Change Get-MSOLUser to Get-Mailbox
    Reworked this script to use Get-Mailbox instead of Get-MSOLUser, since if you have an AD synced Tenant, this will probably have a good number of MSOLUsers that aren't tied to Mailboxes.
    
    Moved big Get-Mailbox pull to top and reused the data
    Added Mailbox param to UPN (to save having to re-lookup whose mailbox it's connected to)
    
    Also, fixes
     (https://github.com/OfficeDev/O365-InvestigationTooling/issues/41)

--- DumpDelegatesandForwardingRulesFromFile.ps1
+++ DumpDelegatesandForwardingRulesFromFile.ps1
@@ -7,8 +7,9 @@ $userCredential = Get-Credential
 #This connects to Azure Active Directory
 Connect-MsolService -Credential $userCredential
 
+#Connecting to Exchange Online
 $ExoSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $userCredential -Authentication Basic -AllowRedirection
-Import-PSSession $ExoSession
+Import-PSSession $ExoSession -DisableNameChecking | Out-Null
 
 
 #Let's Pull in the list of all GUIDS
@@ -19,7 +20,7 @@ $allUsers = @()
 
 foreach ($guid in $UserGuids)
 {
-    $allUsers += Get-MsolUser -ObjectID $guid.ObjectId | select ObjectID, UserPrincipalName, FirstName, LastName, StrongAuthenticationRequirements, StsRefreshTokensValidFrom, StrongPasswordRequired, LastPasswordChangeTimestamp 
+    $allUsers += Get-MsolUser -ObjectID $guid.ObjectId | select-Object ObjectID, UserPrincipalName, FirstName, LastName, StrongAuthenticationRequirements, StsRefreshTokensValidFrom, StrongPasswordRequired, LastPasswordChangeTimestamp 
 }
 
 $UserInboxRules = @()
@@ -28,7 +29,7 @@ $UserDelegates = @()
 foreach ($User in $allUsers)
 {
     Write-Host "Checking inbox rules and delegates for user: " $User.UserPrincipalName;
-    $UserInboxRules += Get-InboxRule -Mailbox $User.UserPrincipalname | Select Name, Description, Enabled, Priority, ForwardTo, ForwardAsAttachmentTo, RedirectTo, DeleteMessage | Where-Object {($_.ForwardTo -ne $null) -or ($_.ForwardAsAttachmentTo -ne $null) -or ($_.RedirectsTo -ne $null)}
+    $UserInboxRules += Get-InboxRule -Mailbox $User.UserPrincipalname | Select-Object @{Name='Mailbox';Expression={$user.UserPrincipalName}}, Name, Description, Enabled, Priority, ForwardTo, ForwardAsAttachmentTo, RedirectTo, DeleteMessage | Where-Object {($_.ForwardTo -ne $null) -or ($_.ForwardAsAttachmentTo -ne $null) -or ($_.RedirectsTo -ne $null)}
     $UserDelegates += Get-MailboxPermission -Identity $User.UserPrincipalName | Where-Object {($_.IsInherited -ne "True") -and ($_.User -notlike "*SELF*")}
 }
 
