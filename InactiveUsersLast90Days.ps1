c5db04c93750ca1eb42bf6fb6361690d39a5d254
esuiru <esteban@irurueta.net>
2019-01-25 11:35:00 -0800

    Update InactiveUsersLast90Days.ps1
    Fixed typo on line 25 "no" to "not"
    Added comments.

--- InactiveUsersLast90Days.ps1
+++ InactiveUsersLast90Days.ps1
@@ -1,25 +1,25 @@
-﻿import-module MSOnline
-
+import-module MSOnline
+ 
 #Let's get us an admin cred!
 $userCredential = Get-Credential
-
-#This connects to Azure Active Directory
+ 
+#This connects to Azure Active Directory and passes admincreds
 Connect-MsolService -Credential $userCredential
 $ExoSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $userCredential -Authentication Basic -AllowRedirection
 Import-PSSession $ExoSession
-
+# Minus 90 Days from Today (Can Change to lower value)
 $startDate = (Get-Date).AddDays(-90).ToString('MM/dd/yyyy')
+#Todays current date
 $endDate = (Get-Date).ToString('MM/dd/yyyy')
-
+#Creates Array for Users
 $allUsers = @()
+#Uses Get-MsolUser cmdlet to get UPN
 $allUsers = Get-MsolUser -All -EnabledFilter EnabledOnly | Select UserPrincipalName
-
+#Creates another Array for dates
 $loggedOnUsers = @()
 $loggedOnUsers = Search-UnifiedAuditLog -StartDate $startDate -EndDate $endDate -Operations UserLoggedIn, PasswordLogonInitialAuthUsingPassword, UserLoginFailed -ResultSize 5000
-
-$inactiveInLastThreeMonthsUsers = @()
-$inactiveInLastThreeMonthsUsers = $allUsers.UserPrincipalName | where {$loggedOnUsers.UserIds -NotContains $_}
-
-Write-Output "The following users have no logged in for the last 90 days:"
-Write-Output $inactiveInLastThreeMonthsUsers
-
+#Creates Array for UPNs not included.
+$inactiveUsers = @()
+$inactiveUsers = $allUsers.UserPrincipalName | where {$loggedOnUsers.UserIds -NotContains $_}
+#Prints Results
+Write-Output "The following users have not logged in for the last 90 days:"
