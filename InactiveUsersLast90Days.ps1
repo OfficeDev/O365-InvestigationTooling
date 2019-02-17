aa59fd425767128a0576af44203417c21ddcaf18
Kevin C <PsychoData91@gmail.com>
2019-02-16 23:07:05 -0500

    Update InactiveUsersLast90Days.ps1
    Add back writing output and formatting

--- InactiveUsersLast90Days.ps1
+++ InactiveUsersLast90Days.ps1
@@ -7,19 +7,25 @@ $userCredential = Get-Credential
 Connect-MsolService -Credential $userCredential
 $ExoSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $userCredential -Authentication Basic -AllowRedirection
 Import-PSSession $ExoSession
+
 # Minus 90 Days from Today (Can Change to lower value)
-$startDate = (Get-Date).AddDays(-90).ToString('MM/dd/yyyy')
+$InactiveDays = -90
+$startDate = (Get-Date).AddDays($InactiveDays).ToString('MM/dd/yyyy')
 #Todays current date
 $endDate = (Get-Date).ToString('MM/dd/yyyy')
+
 #Creates Array for Users
 $allUsers = @()
 #Uses Get-MsolUser cmdlet to get UPN
-$allUsers = Get-MsolUser -All -EnabledFilter EnabledOnly | Select UserPrincipalName
+$allUsers = Get-MsolUser -All -EnabledFilter EnabledOnly | Select-Object UserPrincipalName
+
 #Creates another Array for dates
 $loggedOnUsers = @()
 $loggedOnUsers = Search-UnifiedAuditLog -StartDate $startDate -EndDate $endDate -Operations UserLoggedIn, PasswordLogonInitialAuthUsingPassword, UserLoginFailed -ResultSize 5000
 #Creates Array for UPNs not included.
 $inactiveUsers = @()
-$inactiveUsers = $allUsers.UserPrincipalName | where {$loggedOnUsers.UserIds -NotContains $_}
+$inactiveUsers = $allUsers.UserPrincipalName | Where-Object {$loggedOnUsers.UserIds -NotContains $_}
+
 #Prints Results
 Write-Output "The following users have not logged in for the last 90 days:"
+Write-Output $inactiveUsers
