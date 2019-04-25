21962b99ac71804cdefdd0f13f065ab56d6032d1
Ryan Moore <007@users.noreply.github.com>
2019-04-25 12:18:33 -0700

    Use Identity and try/catch to prevent partial updates
    If any user has a duplicate entry or prefix (my org had the equivalent of `foo` and `foo_1234` mapped to some common identifier) then the `| Set-Mailbox` uses a non-unique index and fails partway through the update with a `multiple objects found` message.
    
    This will fix both problems:
    1) `try` and `catch` to keep going if one mailbox fails
    2) Explicitly use `-Identity` with `DistinguishedName` as a proper UID

--- EnableMailboxAuditing.ps1
+++ EnableMailboxAuditing.ps1
@@ -7,7 +7,17 @@ $ExoSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri
 Import-PSSession $ExoSession -Name Get-Mailbox, Set-Mailbox
 
 #Enable global audit logging
-Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox" -or RecipientTypeDetails -eq "SharedMailbox" -or RecipientTypeDetails -eq "RoomMailbox" -or RecipientTypeDetails -eq "DiscoveryMailbox"} | Set-Mailbox -AuditEnabled $true -AuditLogAgeLimit 180 -AuditAdmin Update, MoveToDeletedItems, SoftDelete, HardDelete, SendAs, SendOnBehalf, Create, UpdateFolderPermission -AuditDelegate Update, SoftDelete, HardDelete, SendAs, Create, UpdateFolderPermissions, MoveToDeletedItems, SendOnBehalf -AuditOwner UpdateFolderPermission, MailboxLogin, Create, SoftDelete, HardDelete, Update, MoveToDeletedItems 
+foreach ($mailbox in Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox" -or RecipientTypeDetails -eq "SharedMailbox" -or RecipientTypeDetails -eq "RoomMailbox" -or RecipientTypeDetails -eq "DiscoveryMailbox"})
+{
+    try
+    {
+        Set-Mailbox -Identity $mailbox.DistinguishedName -AuditEnabled $true -AuditLogAgeLimit 180 -AuditAdmin Update, MoveToDeletedItems, SoftDelete, HardDelete, SendAs, SendOnBehalf, Create, UpdateFolderPermission -AuditDelegate Update, SoftDelete, HardDelete, SendAs, Create, UpdateFolderPermissions, MoveToDeletedItems, SendOnBehalf -AuditOwner UpdateFolderPermission, MailboxLogin, Create, SoftDelete, HardDelete, Update, MoveToDeletedItems
+    }
+    catch
+    {
+        Write-Warning $_.Exception.Message
+    }
+}
 
 #Double-Check It!
 Get-Mailbox -ResultSize Unlimited | Select Name, AuditEnabled, AuditLogAgeLimit | Out-Gridview
