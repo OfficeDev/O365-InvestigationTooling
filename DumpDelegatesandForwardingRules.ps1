dbbc74272c18732b167a3e3c0df997afc6f6759f
Abchrisabc <Chris@The-Eac.nl>
2020-08-03 14:34:33 +0200

    Update DumpDelegatesandForwardingRules.ps1
    Fixed Typo.

--- DumpDelegatesandForwardingRules.ps1
+++ DumpDelegatesandForwardingRules.ps1
@@ -19,7 +19,7 @@ $UserDelegates = @()
 foreach ($User in $allUsers)
 {
     Write-Host "Checking inbox rules and delegates for user: " $User.UserPrincipalName;
-    $UserInboxRules += Get-InboxRule -Mailbox $User.UserPrincipalname | Select Name, Description, Enabled, Priority, ForwardTo, ForwardAsAttachmentTo, RedirectTo, DeleteMessage | Where-Object {($_.ForwardTo -ne $null) -or ($_.ForwardAsAttachmentTo -ne $null) -or ($_.RedirectsTo -ne $null)}
+    $UserInboxRules += Get-InboxRule -Mailbox $User.UserPrincipalname | Select Name, Description, Enabled, Priority, ForwardTo, ForwardAsAttachmentTo, RedirectTo, DeleteMessage | Where-Object {($_.ForwardTo -ne $null) -or ($_.ForwardAsAttachmentTo -ne $null) -or ($_.RedirectTo -ne $null)}
     $UserDelegates += Get-MailboxPermission -Identity $User.UserPrincipalName | Where-Object {($_.IsInherited -ne "True") -and ($_.User -notlike "*SELF*")}
 }
 
